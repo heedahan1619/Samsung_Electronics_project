4fd7389e3bed8167f609367ea9215e5ca4a43e3e
heedahan1619 <heedahan1619@gmail.com>
2024-02-04 13:46:59 +0900

    모듈을 못 찾는 에러 발생

--- naver_stock/naver_stock/spiders/stock_spider.py
+++ naver_stock/naver_stock/spiders/stock_spider.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy import Spider
 from scrapy import Request
 from datetime import datetime, timedelta
-# from items import StockItem
+from items import NaverStockItem
 
 class StockSpider(Spider):
     name = "stock"
@@ -84,10 +84,11 @@ class StockSpider(Spider):
                 if 8 <= i <= 10:
                     continue
                 print(f"\nurl: {url}")
-            
-                date = response.xpath(f"//table[@class='type2']/tr[{i}]/td[1]/span[@class='tah p10 gray03']/text()").get()
-                date = datetime.strptime(date, "%Y.%m.%d")
-                end_price = response.xpath(f"//table[@class='type2']/tr[{i}]/td[2]/span[@class='tah p11']/text()").get()
+                
+                Item = NaverStockItem()
+                Item["date"] = response.xpath(f"//table[@class='type2']/tr[{i}]/td[1]/span[@class='tah p10 gray03']/text()").get()
+                Item["date"] = datetime.strptime(date, "%Y.%m.%d")
+                Item["end_price"] = response.xpath(f"//table[@class='type2']/tr[{i}]/td[2]/span[@class='tah p11']/text()").get()
                 class_name = response.xpath(f"//table[@class='type2']/tr[{i}]/td[3]/span[contains(@class, 'tah p11') or contains(@class, 'tah p11 nv01') or contains(@class, 'tah pa11 red02')]/@class").get()
                 if "nv01" in class_name:
                     prefix = "하락"
@@ -96,16 +97,16 @@ class StockSpider(Spider):
                 else:
                     prefix = ""
                 value = response.xpath(f"//table[@class='type2']/tr[{i}]/td[3]/span[contains(@class, 'tah p11') or contains(@class, 'tah p11 nv01') or contains(@class, 'tah pa11 red02')]/text()").get().strip()
-                change = f"{prefix} {value}"
-                start_price = response.xpath(f"//table[@class='type2']/tr[{i}]/td[4]/span[@class='tah p11']/text()").get()
-                high_price = response.xpath(f"//table[@class='type2']/tr[{i}]/td[5]/span[@class='tah p11']/text()").get()
-                low_price = response.xpath(f"//table[@class='type2']/tr[{i}]/td[6]/span[@class='tah p11']/text()").get()
-                volume = response.xpath(f"//table[@class='type2']/tr[{i}]/td[7]/span[@class='tah p11']/text()").get()
+                Item["change"] = f"{prefix} {value}"
+                Item["start_price"] = response.xpath(f"//table[@class='type2']/tr[{i}]/td[4]/span[@class='tah p11']/text()").get()
+                Item["high_price"] = response.xpath(f"//table[@class='type2']/tr[{i}]/td[5]/span[@class='tah p11']/text()").get()
+                Item["low_price"] = response.xpath(f"//table[@class='type2']/tr[{i}]/td[6]/span[@class='tah p11']/text()").get()
+                Item["volume"] = response.xpath(f"//table[@class='type2']/tr[{i}]/td[7]/span[@class='tah p11']/text()").get()
                 
-                print(f"date: {date}")
-                print(f"end_price: {end_price}")
-                print(f"change: {change}")
-                print(f"start_price: {start_price}")
-                print(f"high_price: {high_price}")
-                print(f"low_price: {low_price}")
-                print(f"volume: {volume}")
+                print(f"date {Item['date']}")
+                print(f"end_price {Item['end_price']}")
+                print(f"change {Item['change']}")
+                print(f"start_price {Item['start_price']}")
+                print(f"high_price {Item['high_price']}")
+                print(f"low_price {Item['low_price']}")
+                print(f"volume {Item['volume']}")
